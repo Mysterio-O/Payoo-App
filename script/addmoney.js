c8d273792f14be348d2452126040be8b0b95c6b8
Mysterio-O <skrabbi.019@gmail.com>
2025-02-25 21:40:14 +0600

    completed home page

--- script/addmoney.js
+++ script/addmoney.js
@@ -1,24 +1,30 @@
-document.getElementById('add-money').addEventListener('click', function(event) {
+document.getElementById('add-money').addEventListener('click', function (event) {
     event.preventDefault();
     const accountNumber = document.getElementById('account-number').value;
     const addAmount = document.getElementById('add-amount').value;
     const pin = document.getElementById('pin').value;
     const mainBalance = document.getElementById('main-balance').innerText;
-    
+
     const convertedAcc = parseInt(accountNumber);
     const convertedAmount = parseFloat(addAmount);
     const convertedPin = parseInt(pin);
     let convertedBalance = parseFloat(mainBalance);
 
-    if(convertedPin === 1234 && convertedAmount > 0 && accountNumber .length === 11) {
-        const newBalance = convertedAmount +  convertedBalance;
-        document.getElementById('main-balance').innerText = newBalance;
+    if (convertedPin && convertedAmount && accountNumber.length) {
+        if (convertedPin === 1234) {
+            const newBalance = convertedAmount + convertedBalance;
+            document.getElementById('main-balance').innerText = newBalance;
+        }
+        else if (convertedAmount <= 0) {
+            alert('Enter Amount');
+        }
+        else {
+        alert('Enter valid PIN')
     }
-    else {
-        alert('something went wrong');
     }
+    
 })
 
-document.getElementById('logout-btn').addEventListener('click', function() {
+document.getElementById('logout-btn').addEventListener('click', function () {
     window.location.href = "./index.html";
 })
